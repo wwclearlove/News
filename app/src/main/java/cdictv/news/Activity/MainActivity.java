f207042711a6f7058b517468205aefa7c7e0cc90
wwclearlove <1280488753@qq.com>
2018-12-17 18:34:48 +0800

    wyc

--- app/src/main/java/cdictv/news/Activity/MainActivity.java
+++ app/src/main/java/cdictv/news/Activity/MainActivity.java
@@ -1,15 +1,17 @@
 package cdictv.news.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import cdictv.news.R;
+import cn.bingoogolapple.bgabanner.BGABanner;
 
 public class MainActivity extends AppCompatActivity {
-
+    BGABanner mBGABanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBGABanner= findViewById(R.id.banner_indicatorId);
     }
 }
