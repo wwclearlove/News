bb89073a120a0719950eeb0f080c32ee0f1ca55d
wwclearlove <1280488753@qq.com>
2018-12-17 17:21:50 +0800

    Initial commit

---
+++ app/src/main/java/cdictv/news/Activity/LoadActivity.java
@@ -0,0 +1,15 @@
+package cdictv.news.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cdictv.news.R;
+
+public class LoadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+    }
+}
