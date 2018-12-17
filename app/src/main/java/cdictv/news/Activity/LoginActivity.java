e9a4f1851ade5db876b0a469dd714e93e9edd4db
Rockly97 <38335896+Rockly97@users.noreply.github.com>
2018-12-17 20:29:57 +0800

    fcx

---
+++ app/src/main/java/cdictv/news/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package cdictv.news.Activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import cdictv.news.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
