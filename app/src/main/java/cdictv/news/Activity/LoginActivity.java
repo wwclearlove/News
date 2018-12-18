bf604f0da55f172efb2f92dbf24121e05e2a810c
Rockly97 <38335896+Rockly97@users.noreply.github.com>
2018-12-18 12:35:14 +0800

    fcx

--- app/src/main/java/cdictv/news/Activity/LoginActivity.java
+++ app/src/main/java/cdictv/news/Activity/LoginActivity.java
@@ -1,16 +1,41 @@
 package cdictv.news.Activity;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import cdictv.news.R;
 
 public class LoginActivity extends Activity {
+
+    @InjectView(R.id.login_user)
+    EditText loginUser;
+    @InjectView(R.id.login_password)
+    EditText loginPassword;
+    @InjectView(R.id.bt_login)
+    TextView btLogin;
+    @InjectView(R.id.bt_resgis)
+    TextView btResgis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.inject(this);
     }
 
+    @OnClick({R.id.bt_login, R.id.bt_resgis})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.bt_login:
+                break;
+            case R.id.bt_resgis:
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/cdictv/news/Utils/TestUtills.java
@@ -0,0 +1,62 @@
+package cdictv.news.Utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 用于验证输入的密码 验证码验证规则
+ *
+ */
+
+public class TestUtills {
+    /**
+     * @param regex
+     *正则表达式的字符串
+     * @param str
+     * 要验证的字符串
+     * @return 如果正确匹配 则返回true 否则返回false
+     */
+    private static boolean match(String regex, String str){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+
+    /**
+     * 验证手机号码是否正确
+     * @param str 要验证的输入的电话号码
+     * @return 返回电话是否正确 如果正确匹配 则返回true 否则返回false
+     */
+    public static boolean IsHandset(String str){
+        String regex = "^[1]+[3,5]+\\d{9}$";
+        return match(regex,str);
+    }
+
+    /**
+     * @param str 输入验证码为4位
+     * @return 验证成功为true
+     */
+    public static boolean IsCodeNmb(String str){
+        String regex = "^[0-9]{4}*$";
+        return match(regex,str);
+    }
+
+    /**
+     * @param str 验证输入6—11位的密码
+     * @return
+     */
+    public static boolean IsPassword(String str){
+        String regex = "^\\d{6,11}$";
+        return match(regex,str);
+    }
+
+    /**
+     * @param str 用户名以字母开头 长度为5-11的用户名
+     * @return
+     */
+    public static boolean IsUserId(String str){
+        String regex = "^[a-z]\\w{4,10}$";
+        return match(regex,str);
+    }
+}
+
